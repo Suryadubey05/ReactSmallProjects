62091ce98664f2eb0fcd78f553b91ca10e3ee13d
surya <suryakant231216@gmail.com>
2024-12-18 20:55:46 +0530

    commit

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,67 @@
+import React, { useRef, useState } from 'react'
+import Card from './Card'
+
+
+function Foreground() {
+
+    const ref = useRef(null);
+
+    const data = [
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, vitae.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, vitae.",
+            filesize: ".9mb",
+            close: false,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, vitae.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, vitae.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, vitae.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, vitae.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, vitae.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue" }
+        }
+    ]
+
+
+
+
+    return (
+        <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+            {data.map((item, index) => (
+                <Card data={item} reference={ref} />
+            ))}
+
+        </div>
+    )
+}
+
+export default Foreground
